dad8d9054dc96a8831e45cdf8585373da5387c80
Karim Osadasdmar <okareem250@gmail.com>
2023-09-02 03:18:06 +0300

    singup teacher page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:education_app/generated/l10n.dart';
 import 'package:education_app/screens/auth/login_screen.dart';
-import 'package:education_app/screens/auth/signup_screen.dart';
+import 'package:education_app/screens/auth/signup_student_screen.dart';
+import 'package:education_app/screens/auth/signup_teacher_screen.dart';
 import 'package:education_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +17,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: Locale("en"),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
-          primaryColor: Colors.cyan,
+          fontFamily: "Poppins",
+          primarySwatch: Colors.cyan,
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.cyan)))),
       home: HomeScreen(),
       routes: {
-"loginscreen":(context)=> LoginScreen(),
-"signupscreen":(context)=> SignUpScreen(),
-
+        "loginscreen": (context) => LoginScreen(),
+        "signupteacher": (context) => SignUpTeacherScreen(),
+        "signupstudent": (context) => SignUpStudentScreen(),
       },
-
     );
   }
 }
